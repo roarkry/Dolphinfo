65e7a6683d61c90b8f05bc3e0ee89f7419917a31
roarkry <roarkry@gmail.com>
2014-12-10 15:07:32 -0600

    Initial Commit
    Committing the first version of Dolphinfo - simple, but room to grow

---
+++ Dolphinfo/Dolphinfo/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dolphinfo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static uint x = 0;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NextImage()
+        {
+            string dirPath = ConfigurationManager.AppSettings["SlideShowSrcDir"];
+            string[] filePaths = Directory.GetFiles(dirPath);
+            string path = string.Empty;
+
+            path = filePaths[x % filePaths.Length];
+            ++x;
+
+            return base.File(path, "image/jpeg");
+        }
+    }
+}
