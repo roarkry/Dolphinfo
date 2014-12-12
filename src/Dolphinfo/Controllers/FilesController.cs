dcf1cc89ddc3b993f06f00f893dbfa97c447348e
roarkry <roarkry@gmail.com>
2014-12-12 16:23:47 -0600

    added more flexible api and better nav
    can now go back and forth, pause with space bar, and use a RESTful API
    for getting at available files and the file itself

---
+++ src/Dolphinfo/Controllers/FilesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Dolphinfo.Controllers
+{
+    public class FilesController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            string dirPath = ConfigurationManager.AppSettings["SlideShowSrcDir"];
+            string[] filePaths = Directory.GetFiles(dirPath);
+            
+            IEnumerable<string> fileNames = filePaths.Select(x => Path.GetFileName(x));
+            
+            return fileNames;
+        }
+
+        public HttpResponseMessage Get(string fileName)
+        {
+            string dirPath = ConfigurationManager.AppSettings["SlideShowSrcDir"];
+            string[] filePaths = Directory.GetFiles(dirPath);
+
+            string path = filePaths.FirstOrDefault(x => Path.GetFileName(x) == fileName);
+
+            HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
+            var stream = new FileStream(path, FileMode.Open);
+            result.Content = new StreamContent(stream);
+            result.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+            return result;
+        }
+    }
+}
--- src/Dolphinfo/Controllers/HomeController.cs
+++ src/Dolphinfo/Controllers/HomeController.cs
@@ -10,23 +10,9 @@ namespace Dolphinfo.Controllers
 {
     public class HomeController : Controller
     {
-        private static uint x = 0;
-
         public ActionResult Index()
         {
             return View();
-        }
-
-        public ActionResult NextImage()
-        {
-            string dirPath = ConfigurationManager.AppSettings["SlideShowSrcDir"];
-            string[] filePaths = Directory.GetFiles(dirPath);
-            string path = string.Empty;
-
-            path = filePaths[x % filePaths.Length];
-            ++x;
-
-            return base.File(path, "image/jpeg");
         }
     }
 }
